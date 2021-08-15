a96a9b5243e39b4c81ebf26cf4dcfe3b44444452
rafalou38 <rafael.martinezcalvo@orange.fr>
2021-08-15 12:30:18 +0200

    implement warlist buttons code

--- src/commands/warnlist.js
+++ src/commands/warnlist.js
@@ -1,16 +1,8 @@
 import Discord from "discord.js";
-import { context } from "../context.js";
+import { context, saveContext } from "../context.js";
 import { getMemberFromText } from "../utils/commands.js";
 
-
-const emojis = [
-	"1️⃣",
-	"2️⃣",
-	"3️⃣",
-	"4️⃣",
-	"5️⃣",
-];
-
+const emojis = ["1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣"];
 
 const warnsCount = 4;
 /**
@@ -21,32 +13,34 @@ const warnsCount = 4;
 function generateEmbed(client, member, index = 0) {
 	let warns = context.warns[member.id];
 	const embed = {
-		"title": `Warns de ${member.user.username}`,
-		"description": `l'utilisateur <@${member.id}> a ${warns.length} warns`,
-		"color": 13849690,
-		"footer": {
-			"icon_url": client.user.avatarURL(),
-			"text": client.user.username
+		title: `Warns de ${member.user.username}`,
+		description: `l'utilisateur <@${member.id}> a ${warns.length} warns`,
+		color: 13849690,
+		footer: {
+			icon_url: client.user.avatarURL(),
+			text: client.user.username,
 		},
-		"thumbnail": {
-			"url": member.user.avatarURL()
+		thumbnail: {
+			url: member.user.avatarURL(),
 		},
-		"author": {
-			"name": client.user.username,
-			"icon_url": client.user.avatarURL(),
+		author: {
+			name: client.user.username,
+			icon_url: client.user.avatarURL(),
 		},
-		"fields": []
+		fields: [],
 	};
 	/** @type {{ value: string; date: string; }[]} */
-	warns.slice(index, index + warnsCount).forEach((warn, i) => {
-		embed.fields.push({
-			name: emojis[i] + " " + warn.date,
-			value: "```" + warn.value + "```"
+	warns
+		.slice(index * warnsCount, index * warnsCount + warnsCount)
+		.forEach((warn, i) => {
+			embed.fields.push({
+				name: emojis[i] + " " + warn.date,
+				value: "```" + warn.value + "```",
+			});
 		});
-	});
 	embed.fields.push({
 		name: "page",
-		value: `${index + 1}/${Math.ceil(warns.length / warnsCount)}`
+		value: `${index + 1}/${Math.ceil(warns.length / warnsCount)}`,
 	});
 
 	return embed;
@@ -58,60 +52,51 @@ function generateEmbed(client, member, index = 0) {
  */
 function generateComponents(index, member) {
 	let warns = context.warns[member.id];
-	warns = warns.slice(index, index + warnsCount);
 	let components = [];
 	let row = new Discord.MessageActionRow()
 		.addComponents(
-			new Discord.MessageButton(
-				{
-
-					customId: "previous",
-					label: "◀",
-					style: "SECONDARY",
-					disabled: index === 0
-				},
-			))
+			new Discord.MessageButton({
+				customId: "previous",
+				label: "◀",
+				style: "SECONDARY",
+				disabled: index === 0,
+			})
+		)
 		.addComponents(
-			new Discord.MessageButton(
-				{
-
-					customId: "clearpage",
-					label: "❌",
-					style: "DANGER",
-				}
-			))
+			new Discord.MessageButton({
+				customId: "ban",
+				label: "❌",
+				style: "DANGER",
+			})
+		)
 		.addComponents(
-			new Discord.MessageButton(
-				{
-
-					customId: "clear",
-					label: "🗑️",
-					style: "DANGER",
-				}
-			))
+			new Discord.MessageButton({
+				customId: "clear",
+				label: "🗑️",
+				style: "DANGER",
+			})
+		)
 		.addComponents(
-			new Discord.MessageButton(
-				{
-
-					customId: "next",
-					label: "▶",
-					style: "SECONDARY",
-				}
-			));
+			new Discord.MessageButton({
+				customId: "next",
+				label: "▶",
+				style: "SECONDARY",
+				disabled:
+					warns.length === 0 ||
+					index + 1 === Math.ceil(warns.length / warnsCount),
+			})
+		);
 	components.push(row);
 	row = new Discord.MessageActionRow();
-
+	warns = warns.slice(index * warnsCount, index * warnsCount + warnsCount);
 	for (let i = 0; i < warnsCount; i++) {
 		row.addComponents(
-			new Discord.MessageButton(
-				{
-
-					customId: (i + 1).toString(),
-					label: emojis[i],
-					style: "DANGER",
-					disabled: !warns[i]
-				},
-			)
+			new Discord.MessageButton({
+				customId: (i + 1).toString(),
+				label: emojis[i],
+				style: "DANGER",
+				disabled: !warns[i],
+			})
 		);
 	}
 	components.push(row);
@@ -119,7 +104,8 @@ function generateComponents(index, member) {
 	return components;
 }
 
-
+/**@type {Discord.InteractionCollector<Discord.ButtonInteraction>} */
+let collector;
 /**
  * ban a user
  *
@@ -130,11 +116,18 @@ function generateComponents(index, member) {
  */
 export default async function (client, message) {
 	const { author, member, content, guild } = message;
-	const args = content.split(" ").slice(1).filter(e => e);
-	const isAdmin = member.roles.cache.some(r => process.env.ADMIN_ROLES.split(", ").includes(r.name));
+	const args = content
+		.split(" ")
+		.slice(1)
+		.filter((e) => e);
+	const isAdmin = member.roles.cache.some((r) =>
+		process.env.ADMIN_ROLES.split(", ").includes(r.name)
+	);
 
 	if (args.length < 1) {
-		await message.reply("mauvais arguments, attendu: ```!warnlist @user```");
+		await message.reply(
+			"mauvais arguments, attendu: ```!warnlist @user```"
+		);
 		return;
 	}
 
@@ -145,14 +138,94 @@ export default async function (client, message) {
 		/**@type {string[]} */
 		const warns = context.warns[target.id] || [];
 
-
+		let i = 0;
 		message.reply({
-			embeds: [
-				generateEmbed(client, target, 0)
-			],
-			components: generateComponents(0, target)
+			embeds: [generateEmbed(client, target, i)],
+			components: generateComponents(i, target),
+		});
+
+		const filter = (i) => true;
+		if (collector) collector.stop();
+		collector = message.channel.createMessageComponentCollector({
+			filter,
+			time: 30_000,
+		});
+
+		collector.on("collect", async (interaction) => {
+			switch (interaction.customId) {
+				case "previous":
+					i--;
+					interaction.update({
+						embeds: [generateEmbed(client, target, i)],
+						components: generateComponents(i, target),
+					});
+					break;
+
+				case "ban":
+					{
+						await interaction.reply("entrez la raison du ban:");
+						const banCollector =
+							message.channel.createMessageCollector({
+								filter: (m) => m.author.id === author.id,
+								time: 15_000,
+							});
+						banCollector.on("collect", async (message) => {
+							target.ban({ reason: message.content });
+
+							interaction.channel.send(
+								`l'utilisateur ${target.user.username} a été banni pour la raison:` +
+									"```" +
+									message.content +
+									"```"
+							);
+
+							banCollector.stop();
+						});
+						banCollector.on("end", async (collected) => {
+							if (collected.size > 0) return;
+
+							interaction.channel.send(
+								"Trop lent annualtion du ban"
+							);
+						});
+					}
+					break;
+
+				case "clear":
+					warns.splice(0, warns.length);
+					interaction.update({
+						embeds: [generateEmbed(client, target, i)],
+						components: generateComponents(i, target),
+					});
+					saveContext();
+					break;
+
+				case "next":
+					i++;
+					interaction.update({
+						embeds: [generateEmbed(client, target, i)],
+						components: generateComponents(i, target),
+					});
+					break;
+
+				default:
+					{
+						if (!interaction.customId.match(/\d/)) return;
+						const warnIndex =
+							4 * i + (parseInt(interaction.customId) - 1);
+						warns.splice(warnIndex, 1);
+						saveContext();
+						interaction.update({
+							embeds: [generateEmbed(client, target, i)],
+							components: generateComponents(i, target),
+						});
+					}
+					break;
+			}
 		});
 	} else {
-		await message.reply("tu n'a pas l'autorisation d'utiliser cette commande");
+		await message.reply(
+			"tu n'a pas l'autorisation d'utiliser cette commande"
+		);
 	}
 }
