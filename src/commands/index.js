95c7c2043a40fab72d114b388b8aae4fdea56b3d
rafalou38 <rafael.martinezcalvo@orange.fr>
2021-08-15 13:19:48 +0200

    add kick command

--- src/commands/index.js
+++ src/commands/index.js
@@ -2,10 +2,12 @@ import help from "./help.js";
 import warn from "./warn.js";
 import warnlist from "./warnlist.js";
 import clear from "./clear.js";
+import kick from "./kick.js";
 
 export const commands = {
 	help,
 	warn,
 	warnlist,
 	clear,
+	kick,
 };
---
+++ src/commands/kick.js
@@ -0,0 +1,41 @@
+import Discord from "discord.js";
+import { getMemberFromText } from "../utils/commands.js";
+
+/**
+ * ban a user
+ *
+ * !ban @utilisateur raison
+ *
+ * @param {Discord.Client<boolean>} client
+ * @param {Discord.Message} message
+ */
+export default async function (client, message) {
+	const { author, member, content, guild } = message;
+	const args = content
+		.split(" ")
+		.slice(1)
+		.filter((e) => e);
+	const isAdmin = member.roles.cache.some((r) =>
+		process.env.ADMIN_ROLES.split(", ").includes(r.name)
+	);
+
+	if (args.length < 1) {
+		await message.reply("mauvais arguments, attendu: ```!kick @user (raison)```");
+		return;
+	}
+
+	let target = await getMemberFromText(guild, client, args[0]);
+	if (!target) return await message.reply(`${args[0]} n'existe pas`);
+	
+	if (isAdmin) {
+		try {
+			await target.kick(args.slice(1).join(" "));
+			await message.reply(`l'utilisateur ${target.displayName} a été kick`);
+		} catch (error) {
+			console.error(error);
+			await message.reply(`Erreur ${target.displayName} n'a pas été kick`);
+		}
+	} else {
+		await message.reply("tu n'a pas l'autorisation d'utiliser cette commande");
+	}
+}
